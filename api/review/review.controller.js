3cf2cf4d6fa68135d921c88c3e4b1fe1844361ee
matan-ode <matanodentz@gmail.com>
2024-11-06 15:35:03 +0200

    Update reviews on ToyDetails

--- api/review/review.controller.js
+++ api/review/review.controller.js
@@ -39,44 +39,60 @@ export async function addReview(req, res) {
     var { loggedinUser } = req
     console.log(loggedinUser)
     try {
-        var review = req.body
-        console.log('review', review);
-
-        // const { userId } = review
-        // review.userId = loggedinUser._id
-        review = await reviewService.add(review)
-
-        //* Give the user credit for adding a review
-        // loggedinUser.score += 10
-        // await userService.update(loggedinUser)
-
-        // Update user score in login token as well
-        const loginToken = authService.getLoginToken(loggedinUser)
-        res.cookie('loginToken', loginToken)
-
-        //* prepare the updated review for sending out
-        review.user = loggedinUser
-        console.log('check id', review.toyId);
-        const newToyId =  review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1")
-        console.log('REVIEW CHECK', newToyId);
-        review.toy = await toyService.getById(newToyId)
-        // review.createdAt = review._id.getTimestamp()
-        // review.toy._id = review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1")
-
-        console.log('REVIEW CHECK', review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1"));
         
-        // delete review.userId
-        // delete review.userId
-        // console.log(review.toy);
-
-        // socketService.broadcast({ type: 'review-added', data: review, userId: loggedinUser._id })
-        // socketService.emitToUser({ type: 'review-about-you', data: review, userId: review.toy._id })
-
-        // const fullUser = await userService.getById(loggedinUser._id)
-        // socketService.emitTo({ type: 'user-updated', data: fullUser, label: fullUser._id })
-        console.log(review);
-
+        var review = req.body
+        review.userId = loggedinUser._id
+        const toyId = review.toyId
+        review = await reviewService.add(review)
+        console.log('2',review);
+        
+        // prepare the updated review for sending out
+        review.user = loggedinUser
+        const toy = await toyService.getById(toyId)
+        review.toy = { name: toy.name, price: toy.price, _id: toy._id }
+        delete review.toyId
+        delete review.userId
         res.send(review)
+        
+        
+        // var review = req.body
+        // console.log('review', review);
+
+        // // const { userId } = review
+        // // review.userId = loggedinUser._id
+        // review = await reviewService.add(review)
+
+        // //* Give the user credit for adding a review
+        // // loggedinUser.score += 10
+        // // await userService.update(loggedinUser)
+
+        // // Update user score in login token as well
+        // const loginToken = authService.getLoginToken(loggedinUser)
+        // res.cookie('loginToken', loginToken)
+
+        // //* prepare the updated review for sending out
+        // review.user = loggedinUser
+        // console.log('check id', review.toyId);
+        // const newToyId =  review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1")
+        // console.log('REVIEW CHECK', newToyId);
+        // review.toy = await toyService.getById(newToyId)
+        // // review.createdAt = review._id.getTimestamp()
+        // // review.toy._id = review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1")
+
+        // console.log('REVIEW CHECK', review.userId.toString().replace(/ObjectId\("(.*)"\)/, "$1"));
+        
+        // // delete review.userId
+        // // delete review.userId
+        // // console.log(review.toy);
+
+        // // socketService.broadcast({ type: 'review-added', data: review, userId: loggedinUser._id })
+        // // socketService.emitToUser({ type: 'review-about-you', data: review, userId: review.toy._id })
+
+        // // const fullUser = await userService.getById(loggedinUser._id)
+        // // socketService.emitTo({ type: 'user-updated', data: fullUser, label: fullUser._id })
+        // console.log(review);
+
+        // res.send(review)
     } catch (err) {
         logger.error('Failed to add review', err)
         res.status(400).send({ err: 'Failed to add review' })
